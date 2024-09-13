5dc16b275a2c71dbe2c399b142a91a6c57a8431c
Fariofa <164511695+M1zeria@users.noreply.github.com>
2024-09-13 10:18:58 -0300

    Create Administradora de cartão.py

---
+++ Administradora de cartão.py
@@ -0,0 +1,6 @@
+faturaMar = float(input())
+faturaNova = faturaMar/2
+faturaAbr = faturaNova*1.065
+print(f"Valor total da fatura: R$ {faturaMar:.2f}")
+print(f"Valor a pagar em Marco: R$ {faturaNova:.2f}")
+print(f"Valor a pagar em Abril: R$ {faturaAbr:.2f}")
